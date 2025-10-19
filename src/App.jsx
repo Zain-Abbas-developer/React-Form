0f8a51812d710bdf4bc89fe02ae4bde418876474
Zain-Abbas-developer <zainabbasllc2@gmail.com>
2025-10-19 11:32:13 -0700

    form start

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>React-Form</h1>
+      <fieldset>
+        <form action="#" method='get'>
+          <label for="firstname">FirstName*</label>
+          <input type="text" name='firstname' id='firstname' placeholder='Enter Your First Name' />
+          <label for="lastname">LastName*</label>
+          <input type="text" name='lastname' id='lastname' placeholder='Enter Your Last Name' />
+          <label for="email">Email*</label>
+          <input type="email" name='email' id='email' placeholder='Enter Email'/>
+          <label for="tel">Contact*</label>
+          <input type="tel" name='contact' id='contact' placeholder='Enter Your Number'/>
+          <label for="gender">Gender*</label>
+          <input type="radio" name='gender' id='male' value="male" /> Male 
+          <input type="radio" name='gender' id='female' value="female" /> Female
+          <input type="radio" name='gender' id='other' value="other" /> Other 
+          <label for="subject">Your best Subject*</label>
+          <input type="checkbox" name='subject' id='english' value="english"/> English
+          <input type="checkbox" name='subject' id="math" value="math"/> Math
+          <input type="checkbox" name='subject' id="physics" value="physics"/> Physics
+          <label for="file">Upload file*</label>
+          <input type="file" name='file' id='file' placeholder='Enter Upload file'/>
+          <label for="url">URL*</label>
+          <input type="url" name='url' id='url' placeholder='Your url' />
+          
+        </form>
+      </fieldset>
+    </div>
   )
 }
 
